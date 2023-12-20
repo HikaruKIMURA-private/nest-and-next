432a4523b442590fb8c3e94278d3449d8aa14ca8
null <hikarudjent@gmail.com>
2023-12-20 22:03:04 +0900

    モジュール追加2

---
+++ backend/src/modules/todos/todos.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { TodosController } from '@/controllers/todos/todos.controller'
+import { TodosModel } from '@/models/todos.model'
+import { TodosService } from '@/services/todos/todos.service'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TodosModel])],
+  controllers: [TodosController],
+  providers: [TodosService],
+})
+export class TodosModule {}
