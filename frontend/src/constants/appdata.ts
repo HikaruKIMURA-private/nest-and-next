100f7613d48ac10bde5691b45ba84570d68c0743
null <hikarudjent@gmail.com>
2023-12-21 21:23:35 +0900

    設定まわり&ざっくりレイアウト

---
+++ frontend/src/constants/appdata.ts
@@ -0,0 +1,11 @@
+/**
+ * アプリ基本情報
+ * @type {Object}
+ */
+export const APP_DATA = {
+  APP_NAME: 'next-and-nest',
+  APP_URL: 'http://localhost:3000/',
+  APP_ICON: '/favicon.ico',
+  APP_DESCRIPTION: 'nestJSとNext.jsで何か作ってみたかったんだ',
+  LOCALE: 'ja',
+}
